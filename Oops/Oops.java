1ad171ec1f54d2d86bc3ef01b7f26ae0a949c9b5
ShivamTaneja <shiivamtaneja@gmail.com>
2024-04-03 12:54:59 +0530

    add this keyword

--- Oops/Oops.java
+++ Oops/Oops.java
@@ -4,12 +4,20 @@ class Person {
   String name;
   int age;
 
-  // static is used so that this variable can be 
+  // static is used so that this variable can be
   // accessed without creating an object of that class
   static int count;
 
   public Person() {
     count++;
+    System.out.println("creating a Person.");
+  }
+
+  public Person(int age, String name) {
+    // this() calls the default constructor
+    this();
+    this.age = age;
+    this.name = name;
   }
 
   void walk() {
@@ -32,9 +40,11 @@ public class Oops {
     p1.age = 0;
     p1.name = "nice";
 
-    p1.eat();
-    p1.walk();
-    p1.walk(10);
+    // p1.eat();
+    // p1.walk();
+    // p1.walk(10);
+
+    Person p2 = new Person(20, "New User");
 
     System.out.println(Person.count);
   }
